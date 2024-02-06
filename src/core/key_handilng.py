39c75d89eab84564398e517cc4843a5bf72ce306
hytsh <celixscondolo@gmail.com>
2024-02-06 15:46:09 -0500

    fix press l from keyhandler and set save cuts press 's'

--- src/core/key_handilng.py
+++ src/core/key_handilng.py
@@ -2,6 +2,7 @@ import cv2
 
 class KeyHandler:
     def __init__(self, editor_instance):
+        # Init the loop for recieve the envents
         self.editor = editor_instance
         self.tracker = self.editor.tracker
         self.img = self.editor.img
@@ -10,12 +11,14 @@ class KeyHandler:
 
 
     def _init_loop(self):
+        # Set the loop
         while self.bool_loop:
             key = cv2.waitKey(12000)
             self._handle_key(key)
 
 
     def _handle_key(self, key):
+        # Verify what event is
         if key == ord('l') or key == ord('L'):
             self._clean_img()
 
@@ -27,22 +30,27 @@ class KeyHandler:
 
 
     def _clean_img(self):
-        self.tracker.save_cuts = []
-        self.tracker.last_buttom = 0
+        # Clean the image for new cuts
+        self.tracker.last_bottom = 0
+        self.tracker.img_pos = 0
         self.tracker.img = self.img.copy()
         self.editor._show_image(self.img)
 
 
     def _cancel_cutter(self):
+        # Cancel and close the windows for the cuts
         self._close_wnds()
 
 
     def _save_cuts(self):
-        self.editor.save_cuts = self.tracker.cuts
+        # Init the save the cuts
         self._close_wnds()
+        self.editor.save_imgs = []
+        self.editor.save_cuts(self.tracker.cuts)
 
 
     def _close_wnds(self):
+        # Close the windows that show the image
         cv2.destroyAllWindows()
         self.bool_loop = False
 
