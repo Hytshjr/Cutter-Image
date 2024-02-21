bd9613c3ee7712ce9a141031127b4023714387ea
hytsh <celixscondolo@gmail.com>
2024-02-21 12:43:38 -0500

    add handle enviroment for project

--- src/core/components_files.py
+++ src/core/components_files.py
@@ -4,8 +4,8 @@ from .exceptions import show_error
 
 class ComponentsProject:
     """Class that handle the components the project"""
-    def __init__(self, project_main_file):
-        self.__project_main_file = project_main_file
+    def __init__(self, project_main_file_path):
+        self.__project_main_file_path = project_main_file_path
         self.__project_dir_parent_path = None
         self.__project_format = None
         self.__project_name = None
@@ -15,10 +15,10 @@ class ComponentsProject:
 
 
     @property
-    def project_main_file(self):
+    def project_main_file_path(self):
         """give the value of private intance"""
 
-        return self.__project_main_file
+        return self.__project_main_file_path
 
     @property
     def project_format(self):
@@ -50,36 +50,26 @@ class ComponentsProject:
         return False
 
 
-    def prepare_for_a_file_format(self, path_new_format_file):
-        """prepare the class for some formatfile"""
-
-        old_path = self.project_main_file
-        new_path = path_new_format_file
-        rename_file(old_path=old_path, new_path=new_path)
-
-        self.update_project_main_file(path_new_format_file)
-
-
-    def update_project_main_file(self, new_project_main_file):
+    def update_project_main_file_path(self, new_project_main_file_path):
         """update the main faile for a change path"""
 
-        self.__project_main_file = new_project_main_file
+        self.__project_main_file_path = new_project_main_file_path
 
 
     def __set_project_dir_parent_path(self):
-        dir_path = os.path.dirname(self.__project_main_file)
+        dir_path = os.path.dirname(self.__project_main_file_path)
 
         if self.verify_path_exist(dir_path):
             self.__project_dir_parent_path = dir_path
 
 
     def __set_project_format(self):
-        extension = os.path.splitext(self.__project_main_file)[1]
+        extension = os.path.splitext(self.__project_main_file_path)[1]
         self.__project_format = extension
 
 
     def __set_project_name(self):
-        parent_path = self.__project_dir_parent_path
+        parent_path = self.project_dir_parent_path
         self.__project_name = os.path.basename(parent_path)
 
 
@@ -87,13 +77,85 @@ class ComponentsProject:
 class ImageFile(ComponentsProject):
     """Representing a image file and components"""
 
-    def __init__(self, project_main_file):
-        super().__init__(project_main_file)
+    def __init__(self, project_main_file_path):
+        super().__init__(project_main_file_path)
         self.__dir_path_save_image_cuts = None
+        self.__image_file_path = self.project_main_file_path
         self.__image_file_name = None
-        self.__image_file_path = None
         self.__set_image_file_name()
-        self.__set_image_file_path()
+        self.__set__dir_path_save_image_cuts()
+
+
+    def __set_image_file_name(self):
+        os.path.split(self.__image_file_path)
+        image_file_name = os.path.split(self.__image_file_path)[1]
+        image_name = os.path.splitext(image_file_name)[0]
+
+        if image_file_name:
+            self.__image_file_name = image_name
+
+
+    def __set__dir_path_save_image_cuts(self):
+        name_dir = (self.project_dir_parent_path, 'image')
+        self.__dir_path_save_image_cuts = os.path.join(*name_dir)
+
+
+    def __make_dir_for_save_image_cuts(self):
+        try:
+            os.mkdir(self.__dir_path_save_image_cuts)
+        except FileExistsError as e:
+            show_error(e)
+
+
+    def __make_file_name(self, cut_number):
+        project_name = self.image_file_name
+        project_format = self.project_format
+        return f'{project_name}_{cut_number}{project_format}'
+
+
+    def __make_file_path(self, file_name):
+        return os.path.join(self.dir_path_save_image_cuts, file_name)
+
+
+    def __create_env_file_image_path(self):
+        parent_dir_path = self.project_dir_parent_path
+        image_file_name = self.project_name + self.project_format
+
+        return os.path.join(parent_dir_path, image_file_name)
+
+
+    def __rename_image_file(self):
+        old_path = self.image_file_path
+        new_path = self.__create_env_file_image_path()
+        rename_file(old_path=old_path, new_path=new_path)
+        return new_path
+
+
+    def init_env_project(self):
+        """init the enviorement for work a dir project"""
+
+        try:
+            new_path = self.__rename_image_file()
+        except FileNotFoundError as error:
+            note_user = "Can't create enviroment for project"
+            show_error(error, note_user)
+        else:
+            self.update_project_main_file_path(new_path)
+            self.__image_file_name = self.project_name
+            self.__image_file_path = new_path
+
+
+    def gen_path_image_cuts(self, amount_image_cuts):
+        """generate the paths for save the image cuts"""
+
+        self.__make_dir_for_save_image_cuts()
+        image_cuts_paths = []
+        for image_cut_number in range(1, amount_image_cuts+1):
+            image_cut_name = self.__make_file_name(image_cut_number)
+            image_cut_path = self.__make_file_path(image_cut_name)
+            image_cuts_paths.append(image_cut_path)
+
+        return image_cuts_paths
 
 
     @property
@@ -117,64 +179,11 @@ class ImageFile(ComponentsProject):
         return self.__image_file_name
 
 
-    def gen_path_image_cuts(self, amount_image_cuts):
-        """generate the paths for save the image cuts"""
-
-        self.__make_dir_for_save_image_cuts()
-        image_cuts_paths = []
-        for image_cut_number in range(1, amount_image_cuts+1):
-            image_cut_name = self.__make_file_name(image_cut_number)
-            image_cut_path = self.__make_file_path(image_cut_name)
-            image_cuts_paths.append(image_cut_path)
-
-        return image_cuts_paths
-
-
-    def __make_file_name(self, cut_number):
-        project_name = self.project_name
-        project_format = self.project_format
-        return f'{project_name}_{cut_number}{project_format}'
-
-
-    def __make_file_path(self, file_name):
-        return os.path.join(self.dir_path_save_image_cuts, file_name)
-
-
-    def __make_dir_for_save_image_cuts(self):
-        dir_path = os.path.join(self.project_dir_parent_path, 'image')
-
-        try:
-            os.mkdir(dir_path)
-        except FileExistsError as e:
-            show_error(e)
-        finally:
-            self.__dir_path_save_image_cuts = dir_path
-
-
-    def __set_image_file_name(self):
-        image_name = self.project_name + self.project_format
-        if image_name:
-            self.__image_file_name = image_name
-
-
-    def __set_image_file_path(self):
-        image_file_path = self.__create_file_image_path()
-        self.__image_file_path = image_file_path
-
-
-    def __create_file_image_path(self):
-        parent_dir_path = self.project_dir_parent_path
-        image_file_name = self.image_file_name
-
-        return os.path.join(parent_dir_path, image_file_name)
-
-
-
 class HtmlFile(ComponentsProject):
     """Representing a image file and components"""
 
-    def __init__(self, project_main_file):
-        super().__init__(project_main_file)
+    def __init__(self, project_main_file_path):
+        super().__init__(project_main_file_path)
         self.__html_file_name = None
         self.__html_file_path = None
         self.__set_html_file_name()
