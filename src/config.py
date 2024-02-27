d3d03864738e8784f4f81e52e4ae15ffb45c6240
hytsh <celixscondolo@gmail.com>
2024-02-26 21:13:12 -0500

    delete print of test xd

--- src/config.py
+++ src/config.py
@@ -5,7 +5,3 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_ROOT = os.path.join(BASE_DIR, 'resources', 'templates')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'src', 'interface', 'static')
-
-print(BASE_DIR)
-print(TEMPLATE_ROOT)
-print(MEDIA_ROOT)
