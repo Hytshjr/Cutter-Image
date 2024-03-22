360fe2bf041e4d0820ee693be01b090944ff6328
hytsh <celixscondolo@gmail.com>
2024-03-22 15:53:48 -0500

    update the setup for executable

--- src/setup.py
+++ src/setup.py
@@ -1,14 +1,15 @@
+"""En terminal: python setup.py build"""
+
 from cx_Freeze import setup, Executable
 
 target = Executable(
    script='main.py',
    base='Win32GUI',  # Esto es importante para no mostrar la consola
-   icon='D://Tottus_App//src//interface//static//images//fazil.png')
+   icon='icon.ico',
+   )
 
-setup(name='Fazil App',
-      version='1.5',
+setup(name='Cut App',
+      version='1.5.1',
       description='Image Cutter',
       executables=[target]
 )
-
-# En terminal: python setup.py build
